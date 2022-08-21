de789d090a8f4d8350432816206c8720d9261f63
emzio <emziolkow@gmail.com>
2022-08-21 19:51:07 +0200

    yaml i dodanie TaskConfigurationProperties

--- src/main/java/pl/coderslab/UdemyToDoRestApi/controller/InfoController.java
+++ src/main/java/pl/coderslab/UdemyToDoRestApi/controller/InfoController.java
@@ -3,6 +3,7 @@ package pl.coderslab.UdemyToDoRestApi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
